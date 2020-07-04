b0d6584ddeee885da3fd4c40724eea10af97a9d7
prash <dprashureddy007@gmail.com>
2020-07-03 18:14:33 -0700

    clean up imports

--- multiclass-crammer-singer.py
+++ multiclass-crammer-singer.py
@@ -1,25 +1,13 @@
 import matplotlib.pyplot as plt 
 import numpy as np
 import random, os
-import time
-import cvxpy as cp
+from time import time
 import pandas as pd
 from sklearn.utils import shuffle
-from random import randrange
 from sklearn.model_selection import train_test_split
-from sklearn.multiclass import OneVsRestClassifier
-from sklearn.svm import LinearSVC
-from sklearn.metrics import classification_report, confusion_matrix
-from sklearn.model_selection import cross_validate, GridSearchCV
-from sklearn.preprocessing import normalize
-from sklearn.svm import SVC
-from sklearn.model_selection import StratifiedKFold
-from sklearn.metrics import SCORERS
 from pystruct.models import MultiClassClf
 from pystruct.learners import (NSlackSSVM, OneSlackSSVM,
                                SubgradientSSVM, FrankWolfeSSVM)
-from time import time
-
 
 
 def load_dataset(base_path, shuffle=True):
