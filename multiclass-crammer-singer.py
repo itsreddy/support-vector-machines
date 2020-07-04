2b9020594a85635ac347fd36e94284569d8ae49a
prash <dprashureddy007@gmail.com>
2020-07-03 17:47:34 -0700

    crammer_singer

--- multiclass-crammer-singer.py
+++ multiclass-crammer-singer.py
@@ -31,60 +31,21 @@ def load_dataset(base_path, shuffle=True):
 
     return raw_df, test_df
 
-def split_data(raw_df, p=0.2):
-    split_index = round((1-p)* len(raw_df))
-    train_df = raw_df[ :split_index]
-    valid_df = raw_df[split_index: ]
-    return train_df, valid_df
+def split_add_bias(raw_df, p=0.2):
+    X = raw_df.drop(labels=['Digit'], axis='columns').to_numpy()
+    y = raw_df['Digit'].to_numpy()
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = p)
+    X_train_bias = np.hstack([X_train, np.ones((X_train.shape[0], 1))])
+    X_test_bias = np.hstack([X_test, np.ones((X_test.shape[0], 1))])
+    # X_bias = np.hstack([X, np.ones((X.shape[0], 1))])
+    return X_train_bias, X_test_bias, y_train, y_test
 
-def create_dfs(train_df, num_classes):
-    dfs = []
-    for i in range(num_classes):
-        temp_df = train_df.loc[raw_df['Digit'] == i]
-        temp_df = temp_df.drop(labels=['Digit'], axis='columns')
-        temp_df = temp_df.reset_index(drop=True)
-        dfs.append(temp_df)
-    return dfs
-
-def ovr_classifier(train_df, num_classes, dim, C=2):
-    weights, bs = [], []
-    dfs = create_dfs(train_df, num_classes)
-    for i in range(num_classes):
-        data1 = dfs[i].to_numpy()
-        rest = []
-        for j in range(num_classes):
-            if j != i:
-                rest.append(dfs[j])
-        restdf = pd.concat(rest, ignore_index=True, sort=False)
-        data2 = restdf.to_numpy()
-
-        D = dim
-        N1 = data1.shape[0]
-        N2 = data2.shape[0]
-
-        data1 = data1 / 100
-        data2 = data2 / 100
-
-        X = np.concatenate((data1, data2), axis=0)
-        y = np.concatenate((np.ones((N1, 1)), - np.ones((N2, 1))), axis=0)
-
-        w = cp.Variable((D, 1))
-        b = cp.Variable()
-        epsilon = cp.Variable((N1+N2, 1))
-        objective = cp.Minimize(cp.sum(cp.square(w))*0.5 + cp.sum(cp.square(epsilon)*C))
-        constraints = [cp.multiply(y, (X @ w + b)) >= 1 - epsilon,  epsilon >= 0]
-
-        prob = cp.Problem(objective, constraints)
-        prob.solve()
-
-        weights.append(w.value.reshape(-1))
-        bs.append(b.value)
+def crammer_singer_classifier(X_train_bias, y_train, C=2):
+    model = MultiClassClf(n_features=X_train_bias.shape[1], n_classes=10)
     
-    W = np.array(weights)
-    B = np.array(bs)
     return W, B
 
-def test(test_df, W, B, dim):
+def test_classifier(test_df, W, B, dim):
     count = 0
     total = test_df.shape[0]
     y_test, y_pred = [], []
